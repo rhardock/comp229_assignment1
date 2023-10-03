9cc36d78bc96ad97c71cf836d72dc4af8fe6885d
Ron <rhardock@gmail.com>
2023-10-03 07:08:25 -0400

    boiler plate code in place

--- routes/old/index.js
+++ routes/old/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Placeholder');
+  res.render('index', {page:'Home', menuId:'home'});
 });
 
 module.exports = router;
